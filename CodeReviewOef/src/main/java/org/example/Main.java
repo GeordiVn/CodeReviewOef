e0c70dc64667752a82b3cf5b5a0f78de18fed383
GeordiVn <r0929633@student.thomasmore.be>
2023-05-05 11:54:49 +0200

    code final

--- CodeReviewOef/src/main/java/org/example/Main.java
+++ CodeReviewOef/src/main/java/org/example/Main.java
@@ -7,9 +7,23 @@ public class Main {
         String password = JOptionPane.showInputDialog("Type uw wachtwoord in: ");
 
         int totalNumbersInPassword = 0;
-
+        boolean hasCaptial = false;
+        boolean hasSpecialChar = false;
         for (char c : password.toCharArray()) {
 
+            boolean isLetter = true;
+            boolean isNumber = true;
+
+            isLetter = Character.isLetter(c);
+            isNumber = Character.isDigit(c);
+
+            if (isNumber == false && isLetter == false) {
+                hasSpecialChar = true;
+            }
+
+            if (Character.isUpperCase(c)) {
+                hasCaptial = true;
+            }
 
             try {
                 Integer.parseInt(String.valueOf(c));
@@ -19,15 +33,20 @@ public class Main {
             }
         }
 
-        System.out.println(totalNumbersInPassword);
+
         if (password.length() < 8) {
             System.out.println("Password must be at least 8 characters!");
         } else if (totalNumbersInPassword < 2) {
-            System.out.println("Password must contain 2 numbers!");
-        } else if (totalNumbersInPassword < 2 || password.length() < 8) {
+            System.out.println("Password must contain at least 2 numbers!");
+
+        } else if (password.length() >= 8 && totalNumbersInPassword < 2) {
             System.out.println("Password must be at least 8 characters and have 2 numbers!!");
+        } else if (hasCaptial == false) {
+            System.out.println("password must contain a capital letter!");
+        } else if (hasSpecialChar == false) {
+            System.out.println("password must have a special character!");
         }
 
-
+        System.out.println("Uw wachtwoord : "+ password + " voldoet aan de eisen!");
     }
 }
